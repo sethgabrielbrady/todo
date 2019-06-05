2efd664f724cf4a07eb8cdd6f120314b462ea61a
Seth Brady <sethgabrielbrady@gmail.com>
2019-06-05 19:53:09 -0400

    added new component

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello, React!'
-), document.getElementById('root'));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Todo = function (_React$Component) {
+  _inherits(Todo, _React$Component);
+
+  _createClass(Todo, [{
+    key: "render",
+    value: function render() {
+
+      return React.createElement(
+        "div",
+        { className: "todo" },
+        React.createElement(
+          "span",
+          null,
+          React.createElement("input", { type: "checkbox", checked: this.state.done }),
+          React.createElement("input", { type: "text", value: this.state.text })
+        )
+      );
+    }
+  }]);
+
+  function Todo(props) {
+    _classCallCheck(this, Todo);
+
+    var _this = _possibleConstructorReturn(this, (Todo.__proto__ || Object.getPrototypeOf(Todo)).call(this, props));
+
+    _this.state = { done: _this.props.done == "true" && props.done,
+      text: props.text
+    };
+    return _this;
+  }
+
+  return Todo;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Todo, { name: "one", done: "false" }), document.getElementById('root'));
--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,24 @@
+class Todo extends React.Component {
+  render(){
+
+    return <div className="todo">
+            <span>
+              <input type="checkbox" checked={this.state.done} />
+              <input type="text" value={this.state.text} />
+            </span>
+           </div>;
+  }
+
+  constructor(props){
+    super(props);
+
+    this.state = { done: (this.props.done == "true" && props.done),
+                     text: props.text
+                   };
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, React!</h1>,
+  <Todo name="one" done="false" />,
   document.getElementById('root')
 );
